bba6d99277de724ca43971eff8cdc8bb7fc325c9
pace-geek <56652686+pace-geek@users.noreply.github.com>
2020-12-27 00:54:17 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css';
 import App from './App';
 import {firebaseConfig} from './config';
 import firebase from 'firebase/app';
